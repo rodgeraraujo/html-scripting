6829db43e3da14cb06940fb6f4ad03e90695755e
Rogério Araújo <rogerio.araujo@mail.com>
2020-05-30 00:40:35 -0300

    test: update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,19 @@
 var pretty = require('pretty');
-const Unknown = require('../src/index');
+const EasyHtml = require('../src/index');
 
-test('instace a new Unknown', () => {
-  var unknown = new Unknown('html');
-  expect(unknown.type).toBe('html');
+test('instace a new EasyHtml', () => {
+  var eHtml = new EasyHtml('html');
+  expect(eHtml.type).toBe('html');
 });
 
 test('generate html5 structure', () => {
-  var unknown = new Unknown('html').html(5, 'en');
-  expect(pretty(unknown.code)).toBe(pretty('<!DOCTYPE html><html lang="en">%head% <body>%container%</body></html>'));
+  var eHtml = new EasyHtml('html').html(5, 'en');
+  expect(pretty(eHtml.code)).toBe(pretty('<!DOCTYPE html><html lang="en">%head% <body>%container%</body></html>'));
 });
 
 test('complete html5 with paragraph', () => {
-  var unknown = new Unknown('html').html(5, 'en').head('UTF-8', 'My title').body().p('My text here');
-
-  expect(pretty(unknown.code)).toBe(
+  console.log(pretty(eHtml.code));
+  expect(pretty(eHtml.code)).toBe(
     pretty(
       '<!DOCTYPE html><html lang="en"><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>My title</title></head> <body><p>My text here</p></body></html>'
     )
